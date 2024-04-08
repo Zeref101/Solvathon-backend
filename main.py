a1d38a8b1ffdb21b5f1a57022a262df40886c22e
Shreyas <shreyas.mohanty03@gmail.com>
2024-04-08 10:28:01 +0530

    Add signup and login endpoints

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Response
 from fastapi.middleware.cors import CORSMiddleware
 import firebase_admin
 from firebase_admin import credentials, auth
 from pydantic import BaseModel
+from routes.authentication import UserSignup, UserLogin, signup, login
+
 
 if not firebase_admin._apps:
     cred = credentials.Certificate("serviceAccountKey.json")
@@ -33,26 +35,13 @@ class UserLogin(BaseModel):
 
 
 @app.post("/signup")
-async def signup(user: UserSignup):
-    try:
-        user_record = auth.create_user(
-            email=user.email,
-            password=user.password
-        )
-        return {"uid": user_record.uid}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+async def signup_endpoint(user: UserSignup):
+    return await signup(user)
 
 
 @app.post("/login")
-async def login(user: UserLogin):
-    try:
-        user_record = auth.get_user_by_email(user.email)
-        if not user_record:
-            raise HTTPException(status_code=400, detail="Invalid credentials")
-        return {"uid": user_record.uid}
-    except Exception as e:
-        raise HTTPException(status_code=400, detail=str(e))
+async def login_endpoint(user: UserLogin, response: Response):
+    return await login(user, response)
 
 
 @app.get("/")
