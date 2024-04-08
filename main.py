dfe9221d9eeb92416f3412dd90589a92ba821140
Shreyas <shreyas.mohanty03@gmail.com>
2024-04-08 16:38:52 +0530

    Add getUser endpoint to retrieve user information

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 import firebase_admin
 from firebase_admin import credentials, auth
 from pydantic import BaseModel
-from routes.authentication import UserSignup, UserLogin, signup, login
+from routes.authentication import UserSignup, UserLogin, signup, login, get_user
 
 
 if not firebase_admin._apps:
@@ -42,6 +42,11 @@ async def signup_endpoint(user: UserSignup):
 @app.post("/login")
 async def login_endpoint(user: UserLogin, response: Response):
     return await login(user, response)
+
+
+@app.get("/user/{uid}")
+async def getUser(uid: str):
+    return await get_user(uid)
 
 
 @app.get("/")
