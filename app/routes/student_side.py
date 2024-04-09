b01483302b556ddf1926e1499f8488b07863b24c
Yash Rathi <yashrathicricket@gmail.com>
2024-04-10 05:09:04 +0530

    amb request

--- app/routes/student_side.py
+++ app/routes/student_side.py
@@ -12,3 +12,19 @@ def order_ambulance(amb_data: AmbulanceRequest):
     ambulance_req_ref = db.collection("ambulance_req")
     ambulance_req_ref.add(amb_data.to_firebase())
     return {"message": "Ambulance request added successfully"}
+
+
+@router.get("/get_ambulance_requests")
+def get_ambulance_requests():
+    db = get_db()
+    ambulance_req_ref = db.collection("ambulance_req")
+    ambulance_req = ambulance_req_ref.stream()
+    return [doc.to_dict() for doc in ambulance_req]
+
+
+@router.get("/get_pending_ambulance_requests")
+def get_pending_ambulance_requests():
+    db = get_db()
+    ambulance_req_ref = db.collection("ambulance_req")
+    ambulance_req = ambulance_req_ref.where("approved", "==", False).stream()
+    return [doc.to_dict() for doc in ambulance_req]
